687c1cd741333e107e0b161df924b3d37dfe32d6
Qva777 <barshay.sasha@gmail.com>
2022-09-24 13:25:25 +0300

    Create schemas models

---
+++ application/schemas.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from enum import Enum
+from typing import Union
+
+from pydantic import BaseModel, validator, root_validator, Field
+
+
+class Status(str, Enum):
+    CREATED = "1"
+    IN_PROGRESS = "2"
+    COMPLETED = "3"
+
+
+class Task(BaseModel):
+    name: Union[str, None] = Field(..., title="The name of the task", max_length=64)
+    description: Union[str, None] = Field(..., title="The description of the item", max_length=250)
+    status: Status
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+    class Config:
+        validate_assignment = True
+
+    @root_validator
+    def number_validator(cls, values):
+        values["updated_at"] = datetime.now()
+        return values
+
+
+# class Manager(BaseModel):
