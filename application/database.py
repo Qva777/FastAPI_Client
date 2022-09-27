3bc6b6cd77802292bb5a3c7ec47cac7a6ba68824
Qva777 <barshay.sasha@gmail.com>
2022-09-27 16:13:55 +0300

    Start m2m

--- application/database.py
+++ application/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -10,3 +14,5 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
--- application/schemas.py
+++ application/schemas.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from enum import Enum
-from typing import Union
+from typing import Union, List
 
-from click import DateTime
+from uuid import UUID
 from pydantic import BaseModel, root_validator, Field, EmailStr
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, sql
+
 
 class Status(int, Enum):
     """Стытусы задач"""
@@ -12,15 +12,39 @@ class Status(int, Enum):
     IN_PROGRESS = 2
     COMPLETED = 3
 
+class Manager(BaseModel):
+    """Шаблон модели менеджера"""
+    # id: int
+    username: Union[str, None] = Field(..., title="Username", max_length=64)
+    first_name: Union[str, None] = Field(..., title="First name", max_length=250)
+    last_name: Union[str, None] = Field(..., title="Last name", max_length=250)
+    email: Union[EmailStr, None] = Field(..., title="Email", )  #############
+    password: Union[str, None] = Field(..., title="Password", max_length=250)
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+
+    class Config:
+        """Предоставления конфигураций"""
+        validate_assignment = True
+        orm_mode = True
+
+    @root_validator
+    def number_validator(cls, values):
+        """Валидация обновлений"""
+        values["updated_at"] = datetime.now()
+        return values
+
 
 class Task(BaseModel):
     """Шаблон модели задач"""
+    # id: int
     name: Union[str, None] = Field(..., title="The name of the task", max_length=64)
     description: Union[str, None] = Field(..., title="The description of the task", max_length=250)
     status: Union[Status] = Field(..., title="The description of the task")
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now()
-
+    # manager: List[Manager]
     class Config:
         """Предоставления конфигураций"""
         validate_assignment = True
@@ -34,33 +58,21 @@ class Task(BaseModel):
             values["updated_at"] = values["created_at"]
         return values
 
-
-
     # @root_validator
     # def number_validator(cls, values):
     #     values["updated_at"] = datetime.now()
     #     return values
 
 
-class Manager(BaseModel):
-    """Шаблон модели менеджера"""
-    username: Union[str, None] = Field(..., title="Username", max_length=64)
-    first_name: Union[str, None] = Field(..., title="First name", max_length=250)
-    last_name: Union[str, None] = Field(..., title="Last name", max_length=250)
-    email: Union[EmailStr, None] = Field(..., title="Email", )#############
-    password: Union[str, None] = Field(..., title="Password", max_length=250)
-    created_at: datetime = datetime.now()
-    updated_at: datetime = datetime.now()
-    # tasks =
 
-    class Config:
-        """Предоставления конфигураций"""
-        validate_assignment = True
-        orm_mode = True
-
-    @root_validator
-    def number_validator(cls, values):
-        """Валидация обновлений"""
-        values["updated_at"] = datetime.now()
-        return values
-
+#
+# class TaskOut(BaseModel):
+#     manager_id: Manager
+#
+#
+# class TaskOut11(BaseModel):
+#     manager_info: List[Manager]
+#
+#
+# class ManagerOut(Manager):
+#     task_info: List[Task]
