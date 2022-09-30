289d29f9914ad9a69c4148dfe93370809e7d78f3
Qva777 <barshay.sasha@gmail.com>
2022-09-30 22:40:09 +0300

    permissions in schemas

--- application/schemas.py
+++ application/schemas.py
@@ -4,13 +4,25 @@ from typing import Union
 from pydantic import BaseModel, root_validator, Field, EmailStr
 
 
+#####################
+from fastapi_permissions import (
+    Allow,
+    Authenticated,
+    Deny,
+    Everyone,
+    configure_permissions,
+    list_permissions,
+)
+
 class Status(int, Enum):
     """Стытусы задач"""
     CREATED = 1
     IN_PROGRESS = 2
     COMPLETED = 3
 
-
+class Roles(Enum):
+    user = "user"
+    admin = "admin"
 class Manager(BaseModel):
     """Шаблон модели менеджера"""
     username: Union[str, None] = Field(..., title="Username", max_length=64)
@@ -20,7 +32,7 @@ class Manager(BaseModel):
     password: Union[str, None] = Field(..., title="Password", max_length=250)
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now()
-
+    principals: Roles = "admin"
     class Config:
         """Предоставления конфигураций"""
         validate_assignment = True
@@ -31,8 +43,23 @@ class Manager(BaseModel):
         """Валидация обновлений"""
         values["updated_at"] = datetime.now()
         return values
-
-
+################################
+    def __acl__(cls):
+        """ defines who can do what to the model instance
+        the function returns a list containing tuples in the form of
+        (Allow or Deny, principal identifier, permission name)
+        If a role is not listed (like "role:user") the access will be
+        automatically deny. It's like a (Deny, Everyone, All) is automatically
+        appended at the end.
+        """
+        return [
+            (Allow, Authenticated, "use"),
+            (Allow, Roles.admin, "use"),
+            (Allow, f"user:{cls.username}", "use"),
+        ]
+class ItemListResource:
+    __acl__ = [(Allow,  "use")]
+#########################
 class Task(BaseModel):
     """Шаблон модели задач"""
     name: Union[str, None] = Field(..., title="The name of the task", max_length=64)
