ded10d0538a9278ce1b761bf7a8ca08545a07bbd
Qva777 <barshay.sasha@gmail.com>
2022-10-04 20:36:49 +0300

    Removing excess

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
 from fastapi_pagination import Page, paginate, add_pagination
-
 from application import models
+from application.auth import get_active_principals, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES,\
+    create_access_token, oauth2_scheme, SECRET_KEY, ALGORITHM
 from application.crud import save_info_manager, search_manager_by_username
-from application.models import ManagerDB
-from application.schemas import Task, Manager, ItemListResource, ManagerInDB, Item, Token
+from application.schemas import Task, Manager, ManagerInDB, Token, ItemListResource
 from application.database import engine, SessionLocal
 
 from sqlalchemy.orm import Session
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import jwt
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from jwt import PyJWTError
-from passlib.context import CryptContext
 from pydantic import ValidationError
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-from fastapi_permissions import (
-    Allow,
-    Authenticated,
-    Deny,
-    Everyone,
-    configure_permissions,
-    list_permissions,
-)
+from fastapi_permissions import configure_permissions
 
 app = FastAPI(title="FastAPI_Client")
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 models.Base.metadata.create_all(bind=engine)
-
-# SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_db():
+    """ Connecting to database """
     try:
         db = SessionLocal()
         yield db
@@ -54,73 +41,25 @@ def get_db():
 #         "principals": ["user:bob", "role:admin"],
 #     },
 # }
-#
-# db = ManagerDB()
 
-#
-# def verify_password(plain_password, hashed_password):
-#     return plain_password == hashed_password
-#
-#
-# def get_user(db, username: str):
-#     if db.username == username:
-#         print("user---", username)
-#     return ManagerDB
-#
-#
-# def get_item(item_id: int):
-#     if item_id in ManagerDB:  ### manager - fake_items_db
-#         item_dict = ManagerDB[item_id]  ### manager - fake_items_db
-#         return Item(**item_dict)
-#
-#
-# def authenticate_user(db, username: str, password: str):
-#     # user = get_user(db, username)
-#     user = db.query(models.ManagerDB).filter(models.ManagerDB.username == username).first()
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
-#
-#
-# def create_access_token(*, data: dict, expires_delta: timedelta):
-#     to_encode = data.copy()
-#     expire = datetime.utcnow() + expires_delta
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
-#
-#
-# async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#     except (PyJWTError, ValidationError):
-#         raise credentials_exception
-#     user = db.query(models.ManagerDB).filter(models.ManagerDB.username == username).first()
-#     if user is None:
-#         raise credentials_exception
-#     return user
-#
-#
-#
-# def get_active_principals(user: Manager = Depends(get_current_user)):
-#     if user:
-#         # user is logged in
-#         principals = [Everyone, Authenticated]
-#         principals.extend(getattr(user, "principals", []))
-#     else:
-#         # user is not logged in
-#         principals = [Everyone]
-#     return principals
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except (PyJWTError, ValidationError):
+        raise credentials_exception
+    user = db.query(models.ManagerDB).filter(models.ManagerDB.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 Permission = configure_permissions(get_active_principals)
@@ -139,88 +78,72 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 # TASK URL
-# @app.get('/api/all-tasks/', response_model=Page[Task], tags=["GET Methods"])
-# async def get_all_tasks( db: Session = Depends(get_db)): #ilr: ItemListResource = Permission("view", ItemListResource),
-#     return paginate(db.query(models.TaskDB).all())
-#
-# add_pagination(app)
-#
-#
-# @app.get('/api/task/{task_id}', tags=["GET Methods"])
-# async def get_task(task_id: int, db: Session = Depends(get_db)):
-#     return db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-#
-#
-# @app.post("/api/create-task", tags=["POST Methods"])
-# async def create_task(task: Task, db: Session = Depends(get_db)):
-#     task_model = models.TaskDB()
-#     task_model.name = task.name
-#     task_model.description = task.description
-#     task_model.status = task.status
-#     task_model.created_at = task.created_at
-#     task_model.updated_at = task.updated_at
-#
-#     # task_model.managers = task.manager
-#
-#     db.add(task_model)
-#     db.commit()
-#
-#     return task
-#
-#
-# @app.put("/api/task/{task_id}", tags=["PUT Methods"])
-# def update_task(task_id: int, task: Task, db: Session = Depends(get_db)):
-#     task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-#     if task_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f"ID {task_id} : Does not exist"
-#         )
-#
-#     task_model.name = task.name
-#     task_model.description = task.description
-#     task_model.status = task.status
-#     # task_model.created_at = task.created_at
-#     task_model.updated_at = task.updated_at
-#     # task_model.managers = managerss.managers
-#
-#     db.add(task_model)
-#     db.commit()
-#
-#     return task
-#
-#
-# @app.delete("/api/task/{task_id}", tags=["DELETE Methods"])
-# def delete_task(task_id: int, db: Session = Depends(get_db)):
-#     task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-#
-#     if task_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f"ID {task_id} : Does not exist"
-#         )
-#
-#     db.query(models.TaskDB).filter(models.TaskDB.id == task_id).delete()
-#
-#     db.commit()
-#
-#
-# # @app.patch("/update")
-# # async def update_profile(task_id: int, task: Task, db: Session = Depends(get_db)):
-# #     task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-# #
-# #     task_model.update(task.dict(exclude_unset=True))
-#
-# # for key, value in TaskDB.items():
-# #     print("key ----", key)
-# #     print("value ----", value)
-# #
-# #     setattr(task, key, value)
-#
-# # db.commit()
-# # return task_model
-#
-# # Manager URL
+@app.get('/api/all-tasks/', response_model=Page[Task], tags=["GET Methods"])
+async def get_all_tasks( db: Session = Depends(get_db)): #ilr: ItemListResource = Permission("view", ItemListResource),
+    return paginate(db.query(models.TaskDB).all())
+
+add_pagination(app)
+
+
+@app.get('/api/task/{task_id}', tags=["GET Methods"])
+async def get_task(task_id: int, db: Session = Depends(get_db)):
+    return db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
+
+
+@app.post("/api/create-task", tags=["POST Methods"])
+async def create_task(task: Task, db: Session = Depends(get_db)):
+    task_model = models.TaskDB()
+    task_model.name = task.name
+    task_model.description = task.description
+    task_model.status = task.status
+    task_model.created_at = task.created_at
+    task_model.updated_at = task.updated_at
+
+    # task_model.managers = task.manager
+
+    db.add(task_model)
+    db.commit()
+
+    return task
+
+
+@app.put("/api/task/{task_id}", tags=["PUT Methods"])
+def update_task(task_id: int, task: Task, db: Session = Depends(get_db)):
+    task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
+    if task_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {task_id} : Does not exist"
+        )
+
+    task_model.name = task.name
+    task_model.description = task.description
+    task_model.status = task.status
+    # task_model.created_at = task.created_at
+    task_model.updated_at = task.updated_at
+    # task_model.managers = managerss.managers
+
+    db.add(task_model)
+    db.commit()
+
+    return task
+
+
+@app.delete("/api/task/{task_id}", tags=["DELETE Methods"])
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
+
+    if task_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {task_id} : Does not exist"
+        )
+
+    db.query(models.TaskDB).filter(models.TaskDB.id == task_id).delete()
+
+    db.commit()
+
+
 
 
 # Manager URLS
@@ -270,3 +193,17 @@ async def get_all_managers(db: Session = Depends(get_db)):
 
 
 add_pagination(app)
+# @app.patch("/update")
+# async def update_profile(task_id: int, task: Task, db: Session = Depends(get_db)):
+#     task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
+#
+#     task_model.update(task.dict(exclude_unset=True))
+
+# for key, value in TaskDB.items():
+#     print("key ----", key)
+#     print("value ----", value)
+#
+#     setattr(task, key, value)
+
+# db.commit()
+# return task_model
