336d8460e08d68f02920f1fb3d441ddc7c34c9cc
Qva777 <barshay.sasha@gmail.com>
2022-10-06 21:47:55 +0300

    Task new file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi_pagination import Page, paginate, add_pagination
 from application import models
-from application.auth import get_active_principals, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES,\
+from application.auth import get_active_principals, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, \
     create_access_token, oauth2_scheme, SECRET_KEY, ALGORITHM
-from application.crud import save_info_manager, search_manager_by_username
+from application.manager_methods import save_info_manager, search_manager_by_username
 from application.schemas import Task, Manager, ManagerInDB, Token, ItemListResource
 from application.database import engine, SessionLocal
 
@@ -18,6 +18,8 @@ from pydantic import ValidationError
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from fastapi_permissions import configure_permissions
+
+from application.task_methods import search_task_by_name, save_info_task
 
 app = FastAPI(title="FastAPI_Client")
 models.Base.metadata.create_all(bind=engine)
@@ -79,71 +81,56 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 # TASK URL
 @app.get('/api/all-tasks/', response_model=Page[Task], tags=["GET Methods"])
-async def get_all_tasks( db: Session = Depends(get_db)):
+async def get_all_tasks(db: Session = Depends(get_db)):
     return paginate(db.query(models.TaskDB).all())
+
 
 add_pagination(app)
 
 
-@app.get('/api/task/{task_id}', tags=["GET Methods"])
-async def get_task(task_id: int, db: Session = Depends(get_db), ilr: ItemListResource = Permission("view", ItemListResource)):
-    return db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
+@app.get('/api/task/{name}', tags=["GET Methods"])
+async def get_task(name: str, db: Session = Depends(get_db),
+                   ilr: ItemListResource = Permission("view", ItemListResource)):
+    return db.query(models.TaskDB).filter(models.TaskDB.name == name).first()
 
 
 @app.post("/api/create-task", tags=["POST Methods"])
 async def create_task(task: Task, db: Session = Depends(get_db)):
     task_model = models.TaskDB()
-    task_model.name = task.name
-    task_model.description = task.description
-    task_model.status = task.status
-    task_model.created_at = task.created_at
-    task_model.updated_at = task.updated_at
-
-    # task_model.managers = task.manager
-
-    db.add(task_model)
-    db.commit()
-
+    save_info_task(task_model, task, db)
     return task
 
 
-@app.put("/api/task/{task_id}", tags=["PUT Methods"])
-def update_task(task_id: int, task: Task, db: Session = Depends(get_db),ilr: ItemListResource = Permission("view", ItemListResource)):
-    task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-    if task_model is None:
-        raise HTTPException(
-            status_code=404,
-            detail=f"ID {task_id} : Does not exist"
-        )
-
-    task_model.name = task.name
-    task_model.description = task.description
-    task_model.status = task.status
-    # task_model.created_at = task.created_at
-    task_model.updated_at = task.updated_at
-    # task_model.managers = managerss.managers
-
-    db.add(task_model)
-    db.commit()
-
-    return task
+# @app.put("/api/task/{task_id}", tags=["PUT Methods"])
+# def update_task(name: str, task: Task, db: Session = Depends(get_db),
+#                 ilr: ItemListResource = Permission("view", ItemListResource)):
+#     task_model = db.query(models.TaskDB).filter(models.TaskDB.name == name).first()
+#     if task_model is None:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=f"Task {name} : Does not exist"
+#         )
+#
+#     task_model.name = task.name
+#     task_model.description = task.description
+#     task_model.status = task.status
+#     # task_model.created_at = task.created_at
+#     task_model.updated_at = task.updated_at
+#     # task_model.managers = managerss.managers
+#
+#     db.add(task_model)
+#     db.commit()
+#
+#     return task
 
 
 @app.delete("/api/task/{task_id}", tags=["DELETE Methods"])
-def delete_task(task_id: int, db: Session = Depends(get_db), ilr: ItemListResource = Permission("view", ItemListResource)):
-    task_model = db.query(models.TaskDB).filter(models.TaskDB.id == task_id).first()
-
-    if task_model is None:
-        raise HTTPException(
-            status_code=404,
-            detail=f"ID {task_id} : Does not exist"
-        )
-
-    db.query(models.TaskDB).filter(models.TaskDB.id == task_id).delete()
-
+def delete_task(name: int, db: Session = Depends(get_db),
+                ilr: ItemListResource = Permission("view", ItemListResource)):
+    """ Delete task by name """
+    search_manager_by_username(name, db)
+    db.query(models.TaskDB).filter(models.TaskDB.name == name).delete()
     db.commit()
-
-
 
 
 # Manager URLS
