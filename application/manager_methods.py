d2d655faff2e65702d9ce79ab5b5aa95f50d5ac6
Qva777 <barshay.sasha@gmail.com>
2022-10-17 19:09:14 +0300

    Manager error validation

--- application/manager_methods.py
+++ application/manager_methods.py
@@ -3,6 +3,7 @@
 from fastapi import HTTPException
 from application import models
 from application.auth import pwd_context
+from application.models import ManagerDB
 
 
 def search_manager_by_username(username, db):
@@ -19,15 +20,20 @@ def search_manager_by_username(username, db):
 
 def save_info_manager(manager_model, manager, db):
     """ Fields that are stored in the manager table in the database """
+
     manager_model.username = manager.username
+    if db.query(models.ManagerDB).filter(models.ManagerDB.username == manager_model.username).first():
+        raise HTTPException(
+            status_code=500,
+            detail=f"This username is already in use"
+        )
     manager_model.hashed_password = pwd_context.hash(manager.hashed_password)
     manager_model.first_name = manager.first_name
     manager_model.last_name = manager.last_name
     manager_model.email = manager.email
     manager_model.created_at = manager.created_at
     manager_model.updated_at = manager.updated_at
-
     # manager_model.tasks = manager.tasks
+
     db.add(manager_model)
     db.commit()
-
