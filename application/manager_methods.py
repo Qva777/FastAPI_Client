8640ae7e801b914535cfd491103ab82914039330
Qva777 <barshay.sasha@gmail.com>
2022-10-26 09:47:41 +0300

    put manager_methods

--- application/manager_methods.py
+++ application/manager_methods.py
@@ -37,3 +37,24 @@ def save_info_manager(manager_model, manager, db):
 
     db.add(manager_model)
     db.commit()
+
+
+def put_info_task(manager_model, manager, db):
+    """ Добавить к менеджеру таск  """
+    manager_model.username = manager.username
+    if db.query(models.ManagerDB).filter(models.ManagerDB.username == manager_model.username).first():
+        raise HTTPException(
+            status_code=500,
+            detail=f"This username is already in use"
+        )
+    manager_model.hashed_password = pwd_context.hash(manager.hashed_password)
+    manager_model.first_name = manager.first_name
+    manager_model.last_name = manager.last_name
+    manager_model.email = manager.email
+    manager_model.created_at = manager.created_at
+    manager_model.updated_at = manager.updated_at
+    # *
+    manager_model.tasks = manager.tasks
+
+    db.add(manager_model)
+    db.commit()
