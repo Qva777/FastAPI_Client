2c88d618e07b935ab907b6957322d9437299db85
Qva777 <barshay.sasha@gmail.com>
2022-10-27 22:44:03 +0300

    Connect m2m

--- application/models.py
+++ application/models.py
@@ -2,13 +2,52 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, sql, Table
 from sqlalchemy.orm import relationship
-from typing import List
 
 from application.database import Base, engine
 
-task_manager = Table("task_manager", Base.metadata,#
-                     Column("task_id", ForeignKey("task.id"), primary_key=True),
-                     Column("manager_id", ForeignKey("manager.id"), primary_key=True))# Промнжуточная таблица
+Base.metadata.create_all(engine)
+
+# # class BookAuthor(Base):
+# #     __tablename__ = 'task_manager'
+# #     task_id = Column(ForeignKey('task.id'), primary_key=True)
+# #     manager_id = Column(ForeignKey('manager.id'), primary_key=True)
+#
+#
+# class ManagerDB(Base):
+#     """Таблица моделей менеджеров в базе данных"""
+#     __tablename__ = 'manager'
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String, unique=True)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     email = Column(String)
+#     hashed_password = Column(String)
+#     created_at = Column(DateTime(timezone=True), server_default=sql.func.now())
+#     updated_at = Column(DateTime(timezone=True), server_default=sql.func.now())
+#
+#     # tasks = relationship("TaskDB", secondary="task_manager", back_populates="manager")  # подключение задачам
+#
+#
+# class TaskDB(Base):
+#     """Таблица моделей задач в базе данных"""
+#     __tablename__ = 'task'
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, unique=True)
+#     description = Column(String)
+#     status = Column(Integer, ForeignKey('task.status'))
+#     created_at = Column(DateTime(timezone=True), server_default=sql.func.now())
+#     updated_at = Column(DateTime(timezone=True), server_default=sql.func.now())
+#
+#     # manager = Column(Integer, ForeignKey('manager.id'))
+#     # managers = relationship("ManagerDB", secondary="task_manager", back_populates="task")  # подключение менеджерам
+#
+
+
+task_manager = Table("task_manager", Base.metadata,  #
+                     Column("task_id", ForeignKey("task.id")),
+                     Column("manager_id", ForeignKey("manager.id")))  # Промнжуточная таблица
+
+
 class ManagerDB(Base):
     """Таблица моделей менеджеров в базе данных"""
     __tablename__ = 'manager'
@@ -20,7 +59,10 @@ class ManagerDB(Base):
     hashed_password = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=sql.func.now())
     updated_at = Column(DateTime(timezone=True), server_default=sql.func.now())
-    tasks = relationship("TaskDB", secondary=task_manager, back_populates="managers")# подключение задачам
+
+    # tasks = relationship("TaskDB", secondary="task_manager")# подключение задачам
+
+
 class TaskDB(Base):
     """Таблица моделей задач в базе данных"""
     __tablename__ = 'task'
@@ -30,6 +72,3 @@ class TaskDB(Base):
     status = Column(Integer, ForeignKey('task.status'))
     created_at = Column(DateTime(timezone=True), server_default=sql.func.now())
     updated_at = Column(DateTime(timezone=True), server_default=sql.func.now())
-    managers = relationship("ManagerDB", secondary=task_manager, back_populates="tasks")# подключение менеджерам
-
-Base.metadata.create_all(engine)
