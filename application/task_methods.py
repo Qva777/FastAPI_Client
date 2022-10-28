cfb99c7b8dfcb72a8e31fec69b55bb0c7af7cead
Qva777 <barshay.sasha@gmail.com>
2022-10-28 21:09:04 +0300

    Update methods

--- application/task_methods.py
+++ application/task_methods.py
@@ -2,6 +2,7 @@
 
 from fastapi import HTTPException
 from application import models
+# from application.models import ManagerDB, TaskDB
 
 
 def search_task_by_name(name, db):
@@ -30,7 +31,6 @@ def save_info_task(task_model, task, db):
     task_model.updated_at = task.updated_at
 
     # task_model.managers = task.managers
-    # task_model.managers = task.managers
 
     db.add(task_model)
     db.commit()
@@ -38,24 +38,29 @@ def save_info_task(task_model, task, db):
 
 def put_info_task(task_model, task, db):
     """ Добавит к таску менеджера """
+    # task_model = TaskDB
+    # task = schemas
+    # manager_model = models.task_manager
+
     task_model.name = task.name
     task_model.description = task.description
     task_model.status = task.status
     task_model.created_at = task.created_at
     task_model.updated_at = task.updated_at
     # *
-    task_model.managers = task.managers
+    # manager_model.manager_id = task.managers
+
 
     db.add(task_model)
     db.commit()
 
 
-def save_info_task_manager(task_manager, task, db):
-    """ Fields that are stored in the task table in the database """
-    for manager in task.managers:
-        task_manager.name = task.name
-        task_manager.username = manager
-        db.add(task_manager)
-        db.commit()
+# def save_info_task_manager(task_manager, task, db):
+#     """ Fields that are stored in the task table in the database """
+#     for manager in task.managers:
+#         task_manager.name = task.name
+#         task_manager.username = manager
+#         db.add(task_manager)
+#         db.commit()
 
     # task_model.managers = task.managers
