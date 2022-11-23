55152cb8a93ea8b0abbbfc54d04b94e320840519
Qva777 <barshay.sasha@gmail.com>
2022-11-23 13:07:44 +0200

    Fix start

--- first_start.sh
+++ first_start.sh
@@ -1,3 +1,3 @@
 python -m venv venv
 venv/bin/active ^
-& cd ../.. & pip install -r requirements.txt & echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
+& pip install -r requirements.txt & echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
