4ef07d80fab350777bb8b4aa58fa569f0f02beb6
Qva777 <barshay.sasha@gmail.com>
2022-11-24 19:58:50 -0300

    Pep8

--- first_start.sh
+++ first_start.sh
@@ -1,3 +1,4 @@
 python -m venv venv
 venv/bin/active ^
-& pip install -r requirements.txt & echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
+& pip install -r requirements.txt ^
+& echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
