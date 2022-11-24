d4ebd3293266ec16373e1a90d76ae2d071ef85c3
Qva777 <barshay.sasha@gmail.com>
2022-11-24 19:58:13 -0300

    Pep8

--- first_start.bat
+++ first_start.bat
@@ -1,3 +1,4 @@
 python -m venv venv
 .\venv\Scripts\activate ^
-& pip install -r requirements.txt & echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
+& pip install -r requirements.txt ^
+& echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
