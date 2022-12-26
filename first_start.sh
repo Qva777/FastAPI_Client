d9d2a350a474ae6a4ba08afc02e120943e6b0952
Qva777 <99911044+Qva777@users.noreply.github.com>
2022-12-26 17:09:26 +0200

    Update first_start.sh
    Change readme.md

--- first_start.sh
+++ first_start.sh
@@ -1,4 +1,4 @@
-python -m venv venv
-venv/bin/active ^
+python3 -m venv venv
+source venv/bin/activate ^
 & pip install -r requirements.txt ^
 & echo SECRET_KEY=YOUR_SECRET_KEY > .env & uvicorn main:app --reload
